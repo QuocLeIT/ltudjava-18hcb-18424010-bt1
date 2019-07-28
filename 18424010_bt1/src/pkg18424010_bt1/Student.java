2f3e3910277596971f975f643c5c3a2f04520288
QuocLeIT <quoc.le.2107@gmail.com>
2019-07-28 17:34:44 +0700

    cắt khoảng trắng dư của biến truyền vào

--- 18424010_bt1/src/pkg18424010_bt1/Student.java
+++ 18424010_bt1/src/pkg18424010_bt1/Student.java
@@ -19,20 +19,34 @@ public class Student {
      public Student(){}
      
      public Student(String _mssv, String _name,String _sex,String _cmnd,String _lop){
-         mssv = _mssv;
-         name = _name;
-         sex = _sex;
-         cmnd = _cmnd;
-         lop = _lop;
+         mssv = _mssv.trim();
+         name = _name.trim();
+         sex = _sex.trim();
+         cmnd = _cmnd.trim();
+         lop = _lop.trim();
      }
      
      public Student(String _lop, String _row){
          String[] words=_row.split(",\\s");
-         mssv = words[1];
-         name = words[2];
-         sex = words[3];
-         cmnd = words[4];
-         lop = _lop;
+         mssv = words[1].trim();
+         name = words[2].trim();
+         sex = words[3].trim();
+         cmnd = words[4].trim();
+         lop = _lop.trim();
      }
      
+     public Student(String _row){
+         String[] words=_row.split(",\\s");
+         mssv = words[0].trim();
+         name = words[1].trim();
+         sex = words[2].trim();
+         cmnd = words[3].trim();
+         lop = words[4].trim();
+     }
+     
+     public String GetInfoStudent(){
+         return mssv + ", " + name + ", " + sex + ", " + cmnd + ", " + lop;
+     }
+     
+    
 }
